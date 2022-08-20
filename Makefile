3911a275042500b83e905497bf96a0ee7d8a1056
hiddevuijk <hdvuijk@gmail.com>
2022-08-20 22:30:24 +0200

    working example in main

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 TARGET = test.exe
 OBJS = main.o
 CC = g++
-CFLAGS = -c -Wall -g -std=c++11
-LFLAGS = -Wall -g
-#CFLAGS = -c -Wall -O3 -DNDEBUG -std=c++11
-#LFLAGS = -Wall -O3 -DNDEBUG
+#CFLAGS = -c -Wall -g -std=c++11
+#LFLAGS = -Wall -g
+CFLAGS = -c -Wall -O3 -DNDEBUG -std=c++11
+LFLAGS = -Wall -O3 -DNDEBUG
 
 $(TARGET): $(OBJS)
 	$(CC) $(LFLAGS) $(OBJS) -o $(TARGET)
 
-main.o: main.cpp vec3.h config_file.h system.h
+main.o: main.cpp vec3.h config_file.h system.h density.h
 	$(CC) $(CFLAGS) main.cpp
 
 
--- main.cpp
+++ main.cpp
@@ -1,38 +1,98 @@
 #include "config_file.h"
 #include "vec3.h"
 #include "system.h"
+#include "density.h"
 
 #include <iostream>
 
 using namespace std;
 
+bool CheckOverlap( vector<Vec3> r, double L)
+{
+  bool overlap = false;
+  int N = r.size();
+  for(int i = 0; i < N; i++) {
+	for(int j = i+1; j < N; j++) {
+      double d = system_helper::distance(r[i], r[j], L);
+      if ( d < 1 ) {
+        overlap = true;
+        break;
+	  }
+	}
+    if (overlap) break;
+  }
 
-class Potential { 
- public:
- Potential(double a) : a(a) {}
- Potential() : a(0) {}
- Vec3 Force(const Vec3& r1, const Vec3& r2) {
-	 return r1 - r2;
+	return overlap;
 }
- double a;
-};
 
 int main()
 {
   Config params("input.txt");
-  unsigned long int seed = params.get_parameter<unsigned long int>("seed");
-  unsigned int number_of_particles = params.get_parameter<unsigned int>("number_of_particles");
-  double system_size_xy = params.get_parameter<double>("system_size_xy");
-  double max_mc_step_size = params.get_parameter<double>("max_mc_step_size");
-  double verlet_list_radius = params.get_parameter<double>("verlet_list_radius");
-  double max_diff = 0.1;
-  Potential potential;
+  unsigned long int seed =
+		params.get_parameter<unsigned long int>("seed");
 
-  System<Potential> system(seed, number_of_particles, system_size_xy,
-					max_mc_step_size, verlet_list_radius,
-					max_diff, potential);
+  unsigned int number_of_particles =
+		params.get_parameter<unsigned int>("number_of_particles");
 
+  double system_size_xy =
+		params.get_parameter<double>("system_size_xy");
+
+  double max_mc_step_size =
+		params.get_parameter<double>("max_mc_step_size");
+
+  double verlet_list_radius =
+		params.get_parameter<double>("verlet_list_radius");
+
+
+  double A = params.get_parameter<double>("A");
+
+  long unsigned int MC_moves_per_sample = params.get_parameter<long unsigned int>("MC_moves_per_sample");
+
+  long unsigned int initial_MC_moves = params.get_parameter<long unsigned int>("initial_MC_moves");
+
+  double zlim = params.get_parameter<double>("zlim");
+  unsigned int number_of_bins =
+		params.get_parameter<double>("number_of_bins");
+
+  unsigned int number_of_samples = params.get_parameter<unsigned int>("number_of_samples");
+
+  System system(seed, number_of_particles, system_size_xy,
+					max_mc_step_size, verlet_list_radius, A);
+
+  double area = system_size_xy * system_size_xy;
+  Density rho_z(-zlim, zlim, number_of_bins, 'z', area);
+
+  system.SavePositions("positions0.dat"); 
+
+
+  //system.MCMoveNoVerletFull(initial_MC_moves);
+  system.MCMoveFull(initial_MC_moves);
+
+  cout << "Init done\n";
+  cout << (1.0 * system.GetNumberOfAcceptedMoves() ) /
+			system.GetNumberOfAttemptedMoves() << endl;
+
+  for (long unsigned int i = 0; i < number_of_samples; i++) {
+	//system.MCMoveNoVerletFull(MC_moves_per_sample);
+	system.MCMoveFull(MC_moves_per_sample);
+	rho_z.Sample(system.GetPositions());
+    cout << number_of_samples << "\t" << i << '\n';
+  }
+
+  cout << (1.0 * system.GetNumberOfAcceptedMoves() ) /
+			system.GetNumberOfAttemptedMoves() << endl;
+
+  if(CheckOverlap(system.GetPositions(), system_size_xy ) ) cout << "FUCK" << endl;
+  rho_z.Save("rhoz.dat");
   system.SavePositions("positions.dat"); 
+
+  cout << system.GetNumberOfAcceptedMoves() * 1.0
+       / (system.GetNumberOfVerletListUpdates() * number_of_particles)
+	   << endl;
+
+  cout << system.GetNumberOfAttemptedMoves() * 1.0
+       / (system.GetNumberOfVerletListUpdates() * number_of_particles)
+	   << endl;
 
   return 0;
 }
