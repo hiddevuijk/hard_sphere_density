793147420429cfe3f1597d730e9213358a160c5e
hiddevuijk <hdvuijk@gmail.com>
2022-08-20 11:08:36 +0200

    system: GetPositions returns whole vector

--- system.h
+++ system.h
@@ -51,7 +51,7 @@ class System {
   void SetPotential(double newA) { A_ = newA; }
   double getPotential() const { return A_; }
 
-  const std::vector<Vec3>& GetPositions() const { return positions_; }
+  std::vector<Vec3> GetPositions() const { return positions_; }
  private:
 	// uniform distribution [-1,1]
 	const boost::uniform_real<double> uniform_distribution_;
